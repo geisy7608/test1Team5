496db7dd6ef68980bfae3fb9d2409cb56b29c2fd
geisy <geisy@DESKTOP-PVAFG3B>
2021-07-09 17:56:45 -0400

    test4-Erreur de Compilation

--- src/test1team5/Test1Team5.java
+++ src/test1team5/Test1Team5.java
@@ -15,9 +15,9 @@ public class Test1Team5 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("Bonjour" + args[1]  + ", " + args[0]);
-        System.out.println("Au revoir");
-        System.out.println("Merci")
+      //  System.out.println("Bonjour" + args[1]  + ", " + args[0]);
+      //  System.out.println("Au revoir");
+        System.out.println("Merci");
     }
     
 }
